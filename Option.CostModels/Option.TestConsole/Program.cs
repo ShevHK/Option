41dd7d591f3589635eb5ed84b190845ebd18254b
Shev <hksheva@gmail.com>
2023-12-13 21:56:59 +0200

    last update

--- Option.CostModels/Option.TestConsole/Program.cs
+++ Option.CostModels/Option.TestConsole/Program.cs
@@ -13,34 +13,37 @@ LevyModel levyModel = new LevyModel();
 
 
 string filePath = @"D:\University\magistry1\diploma\Option.CostModels\symbols\TEST DATA.txt";
+var S = 154.3;
+var K = 160;
+var T = 365;
 
-try
-{
-    string[] lines = File.ReadAllLines(filePath);
+//try
+//{
+//    string[] lines = File.ReadAllLines(filePath);
 
-    foreach (string line in lines)
-    {
-        string[] values = line.Split(',');
+//    foreach (string line in lines)
+//    {
+//        string[] values = line.Split(',');
 
-        string date1 = values[0].Trim();
-        string date2 = values[1].Trim();
-        double stockPrice = Convert.ToDouble(values[2].Trim());
-        double strikePrice = Convert.ToDouble(values[3].Trim());
-        double callPrice = Convert.ToDouble(values[4].Trim());
-        double putPrice = Convert.ToDouble(values[5].Trim());
+//        string date1 = values[0].Trim();
+//        string date2 = values[1].Trim();
+//        double stockPrice = Convert.ToDouble(values[2].Trim());
+//        double strikePrice = Convert.ToDouble(values[3].Trim());
+//        double callPrice = Convert.ToDouble(values[4].Trim());
+//        double putPrice = Convert.ToDouble(values[5].Trim());
 
-        Console.WriteLine($"Дата укладання опціону: {date1}");
-        Console.WriteLine($"Дата виконання: {date2}");
-        Console.WriteLine($"Ціна акції на час укладання: {stockPrice}");
-        Console.WriteLine($"Ціна страйку: {strikePrice}");
-        Console.WriteLine($"Ціна опціону колл: {callPrice}");
-        Console.WriteLine($"Ціна опціону пут: {putPrice}");
+//        Console.WriteLine($"Дата укладання опціону: {date1}");
+//        Console.WriteLine($"Дата виконання: {date2}");
+//        Console.WriteLine($"Ціна акції на час укладання: {stockPrice}");
+//        Console.WriteLine($"Ціна страйку: {strikePrice}");
+//        Console.WriteLine($"Ціна опціону колл: {callPrice}");
+//        Console.WriteLine($"Ціна опціону пут: {putPrice}");
 
-        Console.WriteLine(); // Перехід на новий рядок
+//        Console.WriteLine(); // Перехід на новий рядок
 
-    }
-}
-catch (Exception ex)
-{
-    Console.WriteLine($"Помилка: {ex.Message}");
-}
+//    }
+//}
+//catch (Exception ex)
+//{
+//    Console.WriteLine($"Помилка: {ex.Message}");
+//}
